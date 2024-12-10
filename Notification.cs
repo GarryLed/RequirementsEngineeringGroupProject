1121fa573b2f485b1482c2739208222c0f209ebf
GarryLed <s00256356@atu.ie>
2024-12-10 21:01:03 +0000

    Added more classes

---
+++ Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequirementsEngineeringGroupProject
+{
+    public class Notification
+    {
+        // properties 
+
+
+        // Constructor 
+
+
+
+        // Methods 
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequirementsEngineeringGroupProject
+{
+    internal class Timer
+    {
+        // properties 
+
+
+        // Constructor 
+
+
+
+        // Methods 
+    }
+}
